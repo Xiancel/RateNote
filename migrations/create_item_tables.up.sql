218ed0918d1868e3250644676568b17789ef93c1
Andrey <medvedevandrey860@gmail.com>
2026-03-16 22:02:21 +0200

    init project

---
+++ migrations/create_item_tables.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE items (
+    id UUID PRIMARY KEY,
+    name TEXT NOT NULL,
+    comment TEXT,
+    rating INTEGER NOT NULL CHECK (rating >= 1 AND rating <= 10),
+    image_path TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
