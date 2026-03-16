ac3416a7224e9883987caa5ab27b706ec2ed6730
Andrey <medvedevandrey860@gmail.com>
2026-03-16 22:02:32 +0200

    add item entity

---
+++ internal/entity/item.go
@@ -0,0 +1,17 @@
+package entity
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Item struct {
+	ID        uuid.UUID `db:"id" json:"id"`
+	Name      string    `db:"name" json:"name"`
+	Comment   string    `db:"comment" json:"comment"`
+	Rating    float64   `db:"rating" json:"rating"`
+	ImagePath string    `db:"image_path" json:"image_path"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"update_at"`
+}
