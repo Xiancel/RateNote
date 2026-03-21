49cf2e248e225f81ca0223abf9fd47f6424b39d2
Andrey <medvedevandrey860@gmail.com>
2026-03-21 21:00:34 +0200

    add functions

--- internal/repository/postgres/item_repository.go
+++ internal/repository/postgres/item_repository.go
@@ -53,20 +53,85 @@ func (i *ItemRepo) AddItem(ctx context.Context, item *entity.Item) error {
 
 // DeleteItem implements ItemRepository.
 func (i *ItemRepo) DeleteItem(ctx context.Context, id uuid.UUID) error {
-	panic("unimplemented")
+	query := `
+	DELETE FROM items WHERE id = $1
+	`
+
+	res, err := i.db.ExecContext(ctx, query, id)
+	if err != nil {
+		return fmt.Errorf("failed to delete item: %w", err)
+	}
+
+	rows, _ := res.RowsAffected()
+	if rows == 0 {
+		return fmt.Errorf("items not found")
+	}
+
+	return nil
 }
 
 // GetItemByID implements ItemRepository.
 func (i *ItemRepo) GetItemByID(ctx context.Context, itemID uuid.UUID) (*entity.Item, error) {
-	panic("unimplemented")
+	var item entity.Item
+
+	query := `
+	SELECT id,name,comment,rating,image_path,created_at,updated_at
+	FROM items
+	WHERE id = $1
+	`
+
+	err := i.db.GetContext(ctx, &item, query, itemID)
+	if err != nil {
+		return nil, fmt.Errorf("Items not found: %w", err)
+	}
+
+	return &item, nil
 }
 
 // List implements ItemRepository.
 func (i *ItemRepo) List(ctx context.Context, limit int, offset int) ([]*entity.Item, error) {
-	panic("unimplemented")
+	query := `
+	SELECT id,name,comment,rating,image_path,created_at,updated_at
+	FROM items
+	ORDER BY created_at DESC
+	LIMIT $1 OFFSET $2
+	`
+
+	var items []*entity.Item
+	err := i.db.SelectContext(ctx, &items, query, limit, offset)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list items: %w", err)
+	}
+
+	return items, nil
 }
 
 // UpdateItem implements ItemRepository.
 func (i *ItemRepo) UpdateItem(ctx context.Context, item *entity.Item) error {
-	panic("unimplemented")
+	query := `
+	UPDATE items
+	SET name = $1,
+		comment = $2,
+		rating = $3,
+		image_path = $4,
+		updated_at = NOW()
+	WHERE id = $5
+	`
+
+	res, err := i.db.ExecContext(ctx, query,
+		item.Name,
+		item.Comment,
+		item.Rating,
+		item.ImagePath,
+		item.ID,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to update item: %w", err)
+	}
+	rows, _ := res.RowsAffected()
+	if rows == 0 {
+		return fmt.Errorf("item not found")
+	}
+
+	return nil
 }
