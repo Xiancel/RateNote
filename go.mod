fe73af92aef627b97c7bd483e99f05c813198006
Andrey <medvedevandrey860@gmail.com>
2026-04-02 22:48:49 +0300

    add router and middleware

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module RateNote
 go 1.24.2
 
 require (
+	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.2.5
 	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.4.0
---
+++ internal/handler/http/router.go
@@ -0,0 +1,35 @@
+package http
+
+import (
+	itemService "RateNote/internal/service/item"
+	"net/http"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+)
+
+type RouteConfig struct {
+	ItemService itemService.ItemService
+}
+
+func NewRouter(cfg RouteConfig) *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(CORS)
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		respondJSON(w, http.StatusOK, map[string]string{
+			"status": "ok",
+		})
+	})
+
+	r.Route("/api/v1", func(r chi.Router) {
+		itemHandler := NewItemHandler(cfg.ItemService)
+		itemHandler.RegisterRoutes(r)
+	})
+
+	return r
+}
